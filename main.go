27a04df1d5763bfe93b99b67ef71af9784c9941f
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2024-03-14 10:25:33 -0300

    refactor: new updatings

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	utils "github.com/glener10/rotating-pairs-back/src/common/utils"
+	"github.com/glener10/rotating-pairs-back/src/db"
 	"github.com/glener10/rotating-pairs-back/src/routes"
 )
 
@@ -14,5 +16,13 @@ func main() {
 	}
 
 	r := routes.HandlerRoutes()
+	postgres := &db.Postgres{}
+
+	_, err := postgres.Connect()
+	if err != nil {
+		fmt.Println("Error in Postgres connection: " + err.Error())
+		os.Exit(-1)
+	}
+	defer postgres.Disconnect()
 	routes.Listening(r)
 }
