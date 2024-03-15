a5d2af76ccbfd6e0e8d4cad007f14736cff6d1da
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2024-03-15 08:36:08 -0300

    feat: email format validation

--- src/user/dtos/createUser.go
+++ src/user/dtos/createUser.go
@@ -2,6 +2,7 @@ package user_dtos
 
 import (
 	"errors"
+	"regexp"
 )
 
 type CreateUserRequest struct {
@@ -21,5 +22,14 @@ func Validate(user *CreateUserRequest) error {
 	if user.Password == "" {
 		return errors.New("password is required")
 	}
+	if !IsValidEmail(user.Email) {
+		return errors.New("email is not in the correct format")
+	}
 	return nil
+}
+
+func IsValidEmail(email string) bool {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	regex := regexp.MustCompile(pattern)
+	return regex.MatchString(email)
 }
