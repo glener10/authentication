d74abcd874803f866de678f48c85e1e0089f4aed
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2024-03-17 10:14:18 -0300

    feat: create user_repository interface

---
+++ src/user/interfaces/user_repository.go
@@ -0,0 +1,11 @@
+package user_interfaces
+
+import (
+	user_dtos "github.com/glener10/authentication/src/user/dtos"
+	user_entity "github.com/glener10/authentication/src/user/entities"
+)
+
+type IUserRepository interface {
+	CreateUser(user user_dtos.CreateUserRequest) *user_dtos.CreateUserResponse
+	FindByEmail(email string) *user_entity.User
+}
