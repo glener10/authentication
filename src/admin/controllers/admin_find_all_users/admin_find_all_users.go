f1dc9dd84f8fd6d1b451960ebb0e9f22caa5097f
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2024-04-04 11:20:17 -0300

    feat: new route to find all users by admin jwt

--- src/admin/controllers/admin_find_all_users/admin_find_all_users.go
+++ src/admin/controllers/admin_find_all_users/admin_find_all_users.go
@@ -1,10 +1,9 @@
-package promote_user_admin_controller
+package admin_find_all_users_controller
 
 import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	admin_repositories "github.com/glener10/authentication/src/admin/repositories"
 	admin_usecases "github.com/glener10/authentication/src/admin/usecases"
 	db_postgres "github.com/glener10/authentication/src/db/postgres"
 	log_repositories "github.com/glener10/authentication/src/log/repositories"
@@ -12,20 +11,19 @@ import (
 	user_repositories "github.com/glener10/authentication/src/user/repositories"
 )
 
-// PromoteUserAdmin
-// @Summary Promote user admin (You will need send a JWT token of an administration user in authorization header, you can get it in the login route)
-// @Description Promote user admin by id or email
-// @Tags admin
+// AdminFindAllUsers
+// @Summary Find All Users (You will need send a JWT token of a admin user, you can get it in the login route)
+// @Description Find all users
+// @Tags user
 // @Produce json
 // @Security Bearer
-// @Param find path string true "Search parameter: e-mail or id"
 // @Param Authorization header string true "JWT Token" default(Bearer <token>)
-// @Success 200 {object} nil
+// @Success 200 {object} user_dtos.UserWithoutSensitiveData
 // @Failure      422 {object} utils_interfaces.ErrorResponse
 // @Failure      404 {object} utils_interfaces.ErrorResponse
 // @Failure      401 {object} utils_interfaces.ErrorResponse
-// @Router /admin/promote/{find} [post]
-func PromoteUserAdmin(c *gin.Context) {
+// @Router /user [get]
+func AdminFindAllUsers(c *gin.Context) {
 	parameter := c.Param("find")
 	if err := user_dtos.ValidateFindUser(parameter); err != nil {
 		statusCode := http.StatusUnprocessableEntity
@@ -35,7 +33,6 @@ func PromoteUserAdmin(c *gin.Context) {
 	dbConnection := db_postgres.GetDb()
 	userRepository := &user_repositories.SQLRepository{Db: dbConnection}
 	logRepository := &log_repositories.SQLRepository{Db: dbConnection}
-	adminRepository := &admin_repositories.SQLRepository{Db: dbConnection}
-	useCase := &admin_usecases.PromoteUserAdmin{UserRepository: userRepository, LogRepository: logRepository, AdminRepository: adminRepository}
+	useCase := &admin_usecases.AdminFindUser{UserRepository: userRepository, LogRepository: logRepository}
 	useCase.Executar(c, parameter)
 }
---
+++ src/admin/controllers/admin_find_all_users/admin_find_all_users_test.go
@@ -0,0 +1,84 @@
+package admin_find_all_users_controller
+
+import (
+	"testing"
+
+	db_postgres "github.com/glener10/authentication/src/db/postgres"
+	user_repositories "github.com/glener10/authentication/src/user/repositories"
+	"github.com/glener10/authentication/tests"
+)
+
+var repository user_repositories.SQLRepository
+
+func TestMain(m *testing.M) {
+	tests.SetupDb(m, "file://../../../db/migrations")
+	repository = user_repositories.SQLRepository{Db: db_postgres.GetDb()}
+	tests.ExecuteAndFinish(m)
+}
+
+/*
+func TestAdminFindUserWithJwtOfNonAdminUser(t *testing.T) {
+	tests.BeforeEach()
+	r := tests.SetupRoutes()
+	r.GET("/admin/:find", AdminFindUser)
+
+	req, _ := http.NewRequest("GET", "/admin/1", nil)
+	userForJwt := user_entities.User{
+		Id:       1,
+		Email:    tests.ValidEmail,
+		Password: tests.ValidPassword,
+	}
+	jwtForTest, err := jwt_usecases.GenerateJwt(&userForJwt)
+	if err != nil {
+		log.Fatalf("error to generate jwt in 'TestAdminFindUserWithJwtOfNonAdminUser' test: " + err.Error())
+	}
+	req.Header.Set("Authorization", "Bearer "+*jwtForTest)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+
+	expected := utils_interfaces.ErrorResponse{
+		Error:      "you do not have permission to perform this operation",
+		StatusCode: 401,
+	}
+	var actual utils_interfaces.ErrorResponse
+	err = json.NewDecoder(response.Body).Decode(&actual)
+	if err != nil {
+		t.Errorf("failed to decode response body: %v", err)
+	}
+	assert.Equal(t, response.Result().StatusCode, http.StatusUnauthorized, "should return a 401 status code")
+	assert.Equal(t, expected, actual, "should return 'wyou do not have permission to perform this operation' and 401 in the body")
+}
+
+func TestAdminFindUserWithSuccess(t *testing.T) {
+	tests.BeforeEach()
+	r := tests.SetupRoutes()
+	r.GET("/admin/:find", AdminFindUser)
+	req, _ := http.NewRequest("GET", "/admin/1", nil)
+
+	userToFind := user_dtos.CreateUserRequest{
+		Email:    tests.ValidEmail,
+		Password: tests.ValidPassword,
+	}
+	_, err := repository.CreateUser(userToFind)
+	if err != nil {
+		t.Errorf("failed to create user in 'TestAdminFindUserWithSuccess' test: %v", err)
+	}
+
+	isAdmin := true
+	userAdminForJwt := user_entities.User{
+		Id:       2,
+		IsAdmin:  &isAdmin,
+		Email:    "admin@admin.com",
+		Password: tests.ValidPassword,
+	}
+	jwtForTest, err := jwt_usecases.GenerateJwt(&userAdminForJwt)
+	if err != nil {
+		log.Fatalf("error to generate jwt in 'TestAdminFindUserWithSuccess' test: " + err.Error())
+	}
+	req.Header.Set("Authorization", "Bearer "+*jwtForTest)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+
+	assert.Equal(t, response.Result().StatusCode, http.StatusOK, "should return a 200 status code")
+}
+*/
