55dbc3836a1566bf312cf8c392786b54efb81315
Glener Pizzolato - Pessoal <glenerpizzolato@gmail.com>
2024-04-04 10:19:01 -0300

    feat: usecase to admin_find_user

---
+++ src/admin/usecases/admin_find_user.go
@@ -0,0 +1,75 @@
+package admin_usecases
+
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	jwt_usecases "github.com/glener10/authentication/src/jwt/usecases"
+	log_dtos "github.com/glener10/authentication/src/log/dtos"
+	log_interfaces "github.com/glener10/authentication/src/log/interfaces"
+	log_messages "github.com/glener10/authentication/src/log/messages"
+	user_dtos "github.com/glener10/authentication/src/user/dtos"
+	user_interfaces "github.com/glener10/authentication/src/user/interfaces"
+)
+
+type AdminFindUser struct {
+	UserRepository user_interfaces.IUserRepository
+	LogRepository  log_interfaces.ILogRepository
+}
+
+func (u *AdminFindUser) Executar(c *gin.Context, find string) {
+	authorizationHeader := c.GetHeader("Authorization")
+	jwtFromHeader := strings.Split(authorizationHeader, " ")[1]
+	claims, statusCode, err := jwt_usecases.CheckSignatureAndReturnClaims(jwtFromHeader)
+
+	if err != nil {
+		c.JSON(*statusCode, gin.H{"error": err.Error(), "statusCode": statusCode})
+		go u.AdminFindUserLog(nil, false, log_messages.JWT_INVALID_SIGNATURE, c.ClientIP())
+		return
+	}
+
+	idInClaims := claims["Id"]
+	idInClaimsConvertedToInt := int((idInClaims).(float64))
+	if idInClaims == nil {
+		statusCode := http.StatusBadRequest
+		c.JSON(statusCode, gin.H{"error": "error to map id in claims", "statusCode": statusCode})
+		return
+	}
+
+	isAdminInClaims := claims["IsAdmin"]
+	if isAdminInClaims != true {
+		statusCode := http.StatusUnauthorized
+		c.JSON(statusCode, gin.H{"error": "you do not have permission to perform this operation", "statusCode": statusCode})
+		go u.AdminFindUserLog(&idInClaimsConvertedToInt, false, log_messages.JWT_ADMIN_ELEVATION_REQUIRED, c.ClientIP())
+		return
+	}
+
+	user, err := u.UserRepository.FindUser(find)
+	if err != nil {
+		statusCode := http.StatusNotFound
+		c.JSON(statusCode, gin.H{"error": err.Error(), "statusCode": statusCode})
+		go u.AdminFindUserLog(&idInClaimsConvertedToInt, false, log_messages.FIND_USER_WITHOUT_SUCCESS, c.ClientIP())
+		return
+	}
+
+	userWithoutSensitiveData := user_dtos.UserWithoutSensitiveData{
+		Id:    user.Id,
+		Email: user.Email,
+	}
+	c.JSON(http.StatusOK, userWithoutSensitiveData)
+}
+
+func (u *AdminFindUser) AdminFindUserLog(userId *int, success bool, operationCode string, ip string) {
+	log := &log_dtos.CreateLogRequest{
+		UserId:        userId,
+		Route:         "admin/:find",
+		Method:        "GET",
+		Success:       success,
+		OperationCode: operationCode,
+		Ip:            ip,
+		Timestamp:     time.Now(),
+	}
+	u.LogRepository.CreateLog(*log)
+}
