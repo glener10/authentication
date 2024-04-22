dbda555ff032dedfcd16c5875dd7b0e61755b453
glener10 <glenerpizzolato@gmail.com>
2024-04-22 18:53:42 -0300

    feat: update swagger documentation and controller of new use case

--- src/user/controllers/verify_password_recovery_code/verify_password_recovery_code.go
+++ src/user/controllers/verify_password_recovery_code/verify_password_recovery_code.go
@@ -1,4 +1,4 @@
-package verify_email_controller
+package verify_password_recovery_code_controller
 
 import (
 	"net/http"
@@ -10,21 +10,21 @@ import (
 	user_usecases "github.com/glener10/authentication/src/user/usecases"
 )
 
-// VerifyEmail
-// @Summary Verify Email (You will need send a JWT token in authorization header, you can get it in the login route)
-// @Description Verify Email by e-mail or id
+// VerifyPasswordRecoveryCode
+// @Summary Verify Password Recovery Code (You will need send a JWT token in authorization header, you can get it in the login route)
+// @Description Verify Password Recovery Code by e-mail or id
 // @Tags user
 // @Produce json
 // @Security Bearer
-// @Param tags body user_dtos.Code true "Verify Email"
+// @Param tags body user_dtos.Code true "Password Recovery Code"
 // @Param find path string true "Search parameter: e-mail or id"
 // @Param Authorization header string true "JWT Token" default(Bearer <token>)
 // @Success 200 {object} nil
 // @Failure      422 {object} utils_interfaces.ErrorResponse
 // @Failure      404 {object} utils_interfaces.ErrorResponse
 // @Failure      401 {object} utils_interfaces.ErrorResponse
-// @Router /users/verifyEmail/{find} [post]
-func VerifyEmail(c *gin.Context) {
+// @Router /users/verifyPasswordRecoveryCode/{find} [post]
+func VerifyPasswordRecoveryCode(c *gin.Context) {
 	parameter := c.Param("find")
 	if err := user_dtos.ValidateFindUser(parameter); err != nil {
 		statusCode := http.StatusUnprocessableEntity
