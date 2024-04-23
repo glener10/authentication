4d5c12f790198c937109aa298cce4803f81b613e
glener10 <glenerpizzolato@gmail.com>
2024-04-23 09:24:26 -0300

    feat: change password part in recovery usecase

--- src/user/usecases/change_password_in_recovery.go
+++ src/user/usecases/change_password_in_recovery.go
@@ -3,10 +3,8 @@ package user_usecases
 import (
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/gin-gonic/gin"
-	jwt_usecases "github.com/glener10/authentication/src/jwt/usecases"
 	log_messages "github.com/glener10/authentication/src/log/messages"
 	user_dtos "github.com/glener10/authentication/src/user/dtos"
 	user_interfaces "github.com/glener10/authentication/src/user/interfaces"
@@ -17,41 +15,46 @@ type ChangePasswordInRecovery struct {
 	UserRepository user_interfaces.IUserRepository
 }
 
-func (u *ChangePasswordInRecovery) Executar(c *gin.Context, find string, code user_dtos.ChangePasswordInRecoveryRequest) {
-	authorizationHeader := c.GetHeader("Authorization")
-	jwtFromHeader := strings.Split(authorizationHeader, " ")[1]
-	claims, statusCode, err := jwt_usecases.CheckSignatureAndReturnClaims(jwtFromHeader)
+func (u *ChangePasswordInRecovery) Executar(c *gin.Context, find string, changePasswordInRecoveryRequest user_dtos.ChangePasswordInRecoveryRequest) {
+	num, err := strconv.Atoi(find)
 	if err != nil {
-		c.JSON(*statusCode, gin.H{"error": err.Error(), "statusCode": statusCode})
-		go utils_usecases.CreateLog(nil, "users/changePasswordInRecovery/:find", "POST", false, log_messages.JWT_INVALID_SIGNATURE, c.ClientIP())
+		statusCode := http.StatusInternalServerError
+		c.JSON(statusCode, gin.H{"error": err.Error(), "statusCode": statusCode})
+		go utils_usecases.CreateLog(&num, "users/changePasswordInRecovery/:find", "POST", false, log_messages.CHANGE_PASSWORD_IN_RECOVERY_WITHOUT_SUCCESS, c.ClientIP())
 		return
 	}
 
-	idInClaims := claims["Id"]
-	emailInClaims := claims["Email"]
-	idInClaimsConvertedToInt := int((idInClaims).(float64))
-	if idInClaims == nil || emailInClaims == nil {
-		statusCode := http.StatusBadRequest
-		c.JSON(statusCode, gin.H{"error": "error to map id or email in claims", "statusCode": statusCode})
-		return
-	}
-
-	idFinInNumber, _ := strconv.ParseFloat(find, 64)
-	if idFinInNumber != idInClaims && find != emailInClaims {
-		statusCode := http.StatusUnauthorized
-		c.JSON(statusCode, gin.H{"error": "you do not have permission to perform this operation", "statusCode": statusCode})
-		go utils_usecases.CreateLog(&idInClaimsConvertedToInt, "users/changePasswordInRecovery/:find", "POST", false, log_messages.JWT_UNAUTHORIZED, c.ClientIP())
-		return
-	}
-
-	_, err = u.UserRepository.CheckPasswordRecoveryCode(find, code.Code)
+	_, err = u.UserRepository.CheckPasswordRecoveryCode(find, changePasswordInRecoveryRequest.Code)
 	if err != nil {
 		statusCode := http.StatusUnauthorized
 		c.JSON(statusCode, gin.H{"error": err.Error(), "statusCode": statusCode})
-		go utils_usecases.CreateLog(&idInClaimsConvertedToInt, "users/changePasswordInRecovery/:find", "POST", false, log_messages.VERIFY_PASSWORD_RECOVERY_CODE_WITHOUT_SUCCESS, c.ClientIP())
+		go utils_usecases.CreateLog(&num, "users/changePasswordInRecovery/:find", "POST", false, log_messages.CHANGE_PASSWORD_IN_RECOVERY_WITHOUT_SUCCESS, c.ClientIP())
 		return
 	}
 
-	go utils_usecases.CreateLog(&idInClaimsConvertedToInt, "users/changePasswordInRecovery/:find", "POST", true, log_messages.VERIFY_PASSWORD_RECOVERY_CODE_WITH_SUCCESS, c.ClientIP())
-	c.JSON(http.StatusOK, nil)
+	_, err = u.UserRepository.FindUser(find)
+	if err != nil {
+		statusCode := http.StatusNotFound
+		c.JSON(statusCode, gin.H{"error": err.Error(), "statusCode": statusCode})
+		go utils_usecases.CreateLog(&num, "users/changePasswordInRecovery/:find", "POST", false, log_messages.FIND_USER_WITHOUT_SUCCESS, c.ClientIP())
+		return
+	}
+
+	newPasswordInHash, err := utils_usecases.GenerateHash(changePasswordInRecoveryRequest.NewPassword)
+	if err != nil {
+		statusCode := http.StatusInternalServerError
+		c.JSON(statusCode, gin.H{"error": err.Error(), "statusCode": statusCode})
+		return
+	}
+
+	userWithNewPassword, err := u.UserRepository.ChangePassword(find, *newPasswordInHash)
+	if err != nil {
+		statusCode := http.StatusInternalServerError
+		c.JSON(statusCode, gin.H{"error": err.Error(), "statusCode": statusCode})
+		go utils_usecases.CreateLog(&num, "users/changePasswordInRecovery/:find", "POST", false, log_messages.CHANGE_PASSWORD_WITHOUT_SUCCESS, c.ClientIP())
+		return
+	}
+
+	go utils_usecases.CreateLog(&num, "users/changePasswordInRecovery/:find", "POST", true, log_messages.CHANGE_PASSWORD_IN_RECOVERY_WITH_SUCCESS, c.ClientIP())
+	c.JSON(http.StatusOK, userWithNewPassword)
 }
